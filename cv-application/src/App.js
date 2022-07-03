e37f4f5fae2291d5352d12ac1e250c3856baac63
BrandonC123 <brandoc318@gmail.com>
2022-07-03 13:02:56 -0700

    Create general component to render inputs based on an inputArray.

--- cv-application/src/App.js
+++ cv-application/src/App.js
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import InfoCardBuilder from "./components/InfoCardBuilder";
+import PersonalInformation from "./components/PersonalInformation";
 
 function App() {
+    const [personalInfoArray, setPersonalInfoArray] = useState([
+        { cardTitle: "Personal Info" },
+        { title: "First Name", id: "first-name", type: "text" },
+        { title: "Last Name", id: "last-name", type: "text" },
+        { title: "Address", id: "address", type: "text" },
+        { title: "Phone Number", id: "phone-number", type: "tel" },
+        { title: "Email", id: "email", type: "email" },
+    ]);
+    const [expInfoArray, setExpInfoArray] = useState([
+        { cardTitle: "Experience" },
+        { title: "Position", id: "position", type: "text" },
+        { title: "Company", id: "company", type: "text" },
+    ]);
     return (
         <div className="App">
             <Header />
+            <InfoCardBuilder inputArray={personalInfoArray} />
+            <InfoCardBuilder inputArray={expInfoArray} />
+            {/* <PersonalInformation /> */}
         </div>
     );
 }
--- cv-application/src/components/InfoCardBuilder.jsx
+++ cv-application/src/components/InfoCardBuilder.jsx
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class InfoCardBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pageTitle: "",
+        };
+    }
+    renderInputs = () => {
+        let tempArray = Array.from(this.props.inputArray);
+        // Shift temp array to get title
+        tempArray.shift();
+        // console.log(this.props.inputArray);
+        return tempArray.map((inputContent) => {
+            return (
+                <input
+                    type={inputContent.type}
+                    placeholder={inputContent.title}
+                    id={inputContent.id}
+                ></input>
+            );
+        });
+    };
     render() {
-        return (
-            <div>
-                
-            </div>
-        );
+        return <form>{this.renderInputs()}</form>;
     }
 }
 
