78147391d1fd5b29cba64e734426a562d97342c3
BrandonC123 <brandoc318@gmail.com>
2022-07-03 18:11:50 -0700

    Added code to get all input values to display in view page.

--- cv-application/src/App.js
+++ cv-application/src/App.js
@@ -2,28 +2,53 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import InfoCardBuilder from "./components/InfoCardBuilder";
-import PersonalInformation from "./components/PersonalInformation";
+import View from "./components/View";
 
 function App() {
     const [personalInfoArray, setPersonalInfoArray] = useState([
-        { cardTitle: "Personal Info" },
-        { title: "First Name", id: "first-name", type: "text" },
-        { title: "Last Name", id: "last-name", type: "text" },
-        { title: "Address", id: "address", type: "text" },
-        { title: "Phone Number", id: "phone-number", type: "tel" },
-        { title: "Email", id: "email", type: "email" },
+        { title: "First Name", id: "first-name", type: "text", value: "" },
+        { title: "Last Name", id: "last-name", type: "text", value: "" },
+        { title: "Address", id: "address", type: "text", value: "" },
+        { title: "Phone Number", id: "phone-number", type: "tel", value: "" },
+        { title: "Email", id: "email", type: "email", value: "" },
     ]);
     const [expInfoArray, setExpInfoArray] = useState([
-        { cardTitle: "Experience" },
-        { title: "Position", id: "position", type: "text" },
-        { title: "Company", id: "company", type: "text" },
+        { title: "Position", id: "position", type: "text", value: "" },
+        { title: "Company", id: "company", type: "text", value: "" },
+        { title: "City", id: "city", type: "text", value: "" },
     ]);
+    const [educationArray, setEducationArray] = useState([
+        {
+            title: "University Name",
+            id: "university-name",
+            type: "text",
+            value: "",
+        },
+        { title: "City", id: "city", type: "text", value: "" },
+        { title: "Degree", id: "degree", type: "text", value: "" },
+    ]);
+    const [allInputs, setAllInputs] = useState(
+        personalInfoArray.concat(expInfoArray, educationArray)
+    );
+
+    function getAllInputValues() {
+        let tempArray = Array.from(allInputs);
+        tempArray.map((item) => {
+            item.value = document.getElementById(item.id).value;
+        });
+        setAllInputs(tempArray);
+        console.log(allInputs);
+    }
     return (
         <div className="App">
             <Header />
-            <InfoCardBuilder inputArray={personalInfoArray} />
-            <InfoCardBuilder inputArray={expInfoArray} />
-            {/* <PersonalInformation /> */}
+            <div className="info-card-container">
+                <InfoCardBuilder inputArray={personalInfoArray} />
+                <InfoCardBuilder inputArray={expInfoArray} />
+                <InfoCardBuilder inputArray={educationArray} />
+            </div>
+            <View inputValues={allInputs} />
+            <button onClick={() => getAllInputValues()}>test</button>
         </div>
     );
 }
