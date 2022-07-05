e6125c2e353cf5369f295e8998860e0dc3b5448e
BrandonC123 <brandoc318@gmail.com>
2022-07-04 23:08:12 -0700

    Code header buttons.

--- cv-application/src/components/Header.jsx
+++ cv-application/src/components/Header.jsx
@@ -3,11 +3,40 @@ import React, { Component } from "react";
 class Header extends Component {
     render() {
         return (
-            <header className="header">
+            <header className="header row">
                 <h1 className="header-title">CV Creator</h1>
-                <nav>
-                    <li>Build</li>
-                    <li>View</li>
+                <nav className="row">
+                    <li>
+                        <button
+                            onClick={() => {
+                                document
+                                    .querySelector(".info-card-container")
+                                    .classList.remove("hide");
+                                document
+                                    .querySelector(".view-container")
+                                    .classList.add("hide");
+                            }}
+                            className="build-btn"
+                        >
+                            Build
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={() => {
+                                document
+                                    .querySelector(".info-card-container")
+                                    .classList.add("hide");
+                                document
+                                    .querySelector(".view-container")
+                                    .classList.remove("hide");
+                                this.props.getAllInputValues();
+                            }}
+                            className="link-btn"
+                        >
+                            View
+                        </button>
+                    </li>
                 </nav>
             </header>
         );
