f8cd24e93ce245a46918f62097f011160babd7ed
BrandonC123 <brandoc318@gmail.com>
2022-07-04 21:48:38 -0700

    Added test code for adding more than one experience section.

--- cv-application/src/App.js
+++ cv-application/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import InfoCardBuilder from "./components/InfoCardBuilder";
 import View from "./components/View";
+import uniqid from "uniqid";
 
 function App() {
     const [personalInfoArray, setPersonalInfoArray] = useState([
@@ -11,12 +12,19 @@ function App() {
         { title: "Address", id: "address", type: "text", value: "" },
         { title: "Phone Number", id: "phone-number", type: "tel", value: "" },
         { title: "Email", id: "email", type: "email", value: "" },
-        { title: "Description", id: "description", type: "textarea", value: "" },
+        {
+            title: "Description",
+            id: "description",
+            type: "textarea",
+            value: "",
+        },
     ]);
     const [expInfoArray, setExpInfoArray] = useState([
-        { title: "Position", id: "position", type: "text", value: "" },
-        { title: "Company", id: "company", type: "text", value: "" },
-        { title: "City", id: "work-city", type: "text", value: "" },
+        [
+            { title: "Position", id: uniqid(), type: "text", value: "" },
+            { title: "Company", id: uniqid(), type: "text", value: "" },
+            { title: "City", id: uniqid(), type: "text", value: "" },
+        ],
     ]);
     const [educationArray, setEducationArray] = useState([
         {
@@ -28,8 +36,14 @@ function App() {
         { title: "City", id: "school-city", type: "text", value: "" },
         { title: "Degree", id: "degree", type: "text", value: "" },
     ]);
+    const [expInfoCards, setExpInfoCards] = useState([
+        <InfoCardBuilder
+            cardTitle={"Experience"}
+            inputArray={expInfoArray[0]}
+        />,
+    ]);
     const [allInputs, setAllInputs] = useState(
-        personalInfoArray.concat(expInfoArray, educationArray)
+        personalInfoArray.concat(educationArray)
     );
 
     function getAllInputValues() {
@@ -37,7 +51,22 @@ function App() {
         tempArray.forEach((item) => {
             item.value = document.getElementById(item.id).value;
         });
+        let tempArray2 = Array.from(expInfoArray);
+        tempArray2.forEach((array) => {
+            array.forEach((item) => {
+                item.value = document.getElementById(item.id).value;
+            });
+        });
         setAllInputs(tempArray);
+    }
+    function addNewExpSection() {
+        const tempArray = [
+            { title: "Position", id: uniqid(), type: "text", value: "" },
+            { title: "Company", id: uniqid(), type: "text", value: "" },
+            { title: "City", id: uniqid(), type: "text", value: "" },
+        ];
+        setExpInfoArray([...expInfoArray, tempArray]);
+        console.log(expInfoArray);
     }
     return (
         <div className="App">
@@ -47,18 +76,15 @@ function App() {
                     cardTitle={"Personal Information"}
                     inputArray={personalInfoArray}
                 />
-                <InfoCardBuilder
-                    cardTitle={"Experience"}
-                    inputArray={expInfoArray}
-                />
+                {expInfoCards}
                 <InfoCardBuilder
                     cardTitle={"Education"}
                     inputArray={educationArray}
                 />
-                <button>Submit</button>
+                <button onClick={() => addNewExpSection()}>Submit</button>
             </div>
             <div className="view-container container hide">
-                <View inputValues={allInputs} />
+                <View inputValues={allInputs} expInfoArray={expInfoArray} />
             </div>
         </div>
     );
